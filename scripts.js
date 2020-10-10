4c3564c90c7d9ee1693e2154845d08bc076d96cf
fatmah <fatmah.al.123.123@gmai.com>
2020-10-10 21:02:06 +0300

    last

--- scripts.js
+++ scripts.js
@@ -7,7 +7,23 @@
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+function plant() {
+    let title = document.getElementById("mno");
+    let img =document.getElementById("img");
+    let body = document.getElementById("body");
+    if (title.innerHTML == "الشمس") {
+        
+   
 
+    title.innerHTML="القمر"
+    img.src ="./images/moon.jpg" 
+    body.style.background= '#4d79ff'
+}else{
+    title.innerHTML="الشمس"
+    img.src ="./images/sun.jpg" 
+    body.style.background= '#e65c00'
+    
+}
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
 
@@ -22,3 +38,4 @@
     تتغير الصورة لصورة الشمس
     "يتغير الاسم أسفل الصورة ويصبح "الشمس
 */
+}
